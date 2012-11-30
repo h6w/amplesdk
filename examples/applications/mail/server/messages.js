c25135b19a6739931245af1a75dd4a85f9bd11ec
Tudor Holton <tudor@tudorholton.com>
2012-12-01 09:46:16 +1100

    Modified mail client example to demo observes onbroadcast implementation.

--- examples/applications/mail/server/messages.js
+++ examples/applications/mail/server/messages.js
@@ -1,12 +1,13 @@
 [
-	{"id":"1",	"f":"sam@test.com",	"h":"My message", "l":"meeting today",	"a":"", "t":"1990-12-05T12:33"},
-	{"id":"2",	"f":"sam@test.com",	"h":"My message", "l":"meeting today", "a":"", "t":"1990-12-05T12:33"},
-	{"id":"3",	"f":"sam@test.com",	"h":"My message", "l":"meeting today", "a":"", "t":"1990-12-05T12:33"},
-	{"id":"4",	"f":"sam@test.com",	"h":"My message", "l":"meeting today", "a":"", "t":"1990-12-05T12:33"},
-	{"id":"5",	"f":"sam@test.com",	"h":"My message", "l":"meeting today", "a":"", "t":"1990-12-05T12:33"},
-	{"id":"6",	"f":"sam@test.com",	"h":"My message", "l":"meeting today", "a":"", "t":"1990-12-05T12:33"},
-	{"id":"7",	"f":"sam@test.com",	"h":"My message", "l":"meeting today", "a":"", "t":"1990-12-05T12:33"},
-	{"id":"8",	"f":"sam@test.com",	"h":"My message", "l":"meeting today", "a":"", "t":"1990-12-05T12:33"},
-	{"id":"9",	"f":"sam@test.com",	"h":"My message", "l":"meeting today", "a":"", "t":"1990-12-05T12:33"},
-	{"id":"10",	"f":"sam@test.com",	"h":"My message", "l":"meeting today", "a":"", "t":"1990-12-05T12:33"}
+	{"id":"1",	"f":"lancelot@camelot.com",	"h":"I think you're pretty nice.", "l":"meeting today", "a":"", "t":"1990-12-05T12:33", "c":"Message contents"},
+	{"id":"2",	"f":"lancelot@camelot.com",	"h":"I think you're pretty.", "l":"meeting today", "a":"", "t":"1990-12-05T12:34", "c":"Message contents"},
+	{"id":"3",	"f":"lancelot@camelot.com",	"h":"Please spent some time with me.", "l":"meeting today", "a":"", "t":"1990-12-05T12:35", "c":"Message contents"},
+	{"id":"4",	"f":"lancelot@camelot.com",	"h":"I think I love you.", "l":"meeting today", "a":"", "t":"1990-12-05T12:36", "c":"Message contents"},
+	{"id":"5",	"f":"meliagaunt@camelot.com",	"h":"I know what you did last summer.", "l":"meeting today", "a":"", "t":"1990-12-05T12:37", "c":"Message contents"},
+	{"id":"6",	"f":"lancelot@camelot.com",	"h":"What did you tell Meliagaunt?", "l":"meeting today", "a":"", "t":"1990-12-05T12:38", "c":"Message contents"},
+	{"id":"7",	"f":"lancelot@camelot.com",	"h":"I shall deal with him, my love.", "l":"meeting today", "a":"", "t":"1990-12-05T12:39", "c":"Message contents"},
+	{"id":"8",	"f":"lancelot@camelot.com",	"h":"I hath cleaved him in two.", "l":"meeting today", "a":"", "t":"1990-12-05T12:40", "c":"Message contents"},
+	{"id":"9",	"f":"agravain@camelot.com",	"h":"How could you?", "l":"meeting today", "a":"", "t":"1990-12-05T12:41", "c":"Message contents"},
+	{"id":"10",	"f":"lancelot@camelot.com",	"h":"Which nunnery did you go to?", "l":"meeting today", "a":"", "t":"1990-12-05T12:42", "c":"Message contents"},
+	{"id":"11",	"f":"alfred@tennyson.com",	"h":"Your pain is immortalised.", "l":"meeting today", "a":"", "t":"1990-12-05T12:43", "c":"Message contents"}
 ]
