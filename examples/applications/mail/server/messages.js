d1925c6eaed8aae4e1d48bc82d8015712808794b
h6w <tudor@tudorholton.com>
2015-01-28 10:45:10 +1100

    Update messages.js
    Typo

--- examples/applications/mail/server/messages.js
+++ examples/applications/mail/server/messages.js
@@ -1,7 +1,7 @@
 [
 	{"id":"1",	"f":"lancelot@camelot.com",	"h":"I think you're pretty nice.", "l":"meeting today", "a":"", "t":"1990-12-05T12:33", "c":"Message contents"},
 	{"id":"2",	"f":"lancelot@camelot.com",	"h":"I think you're pretty.", "l":"meeting today", "a":"", "t":"1990-12-05T12:34", "c":"Message contents"},
-	{"id":"3",	"f":"lancelot@camelot.com",	"h":"Please spent some time with me.", "l":"meeting today", "a":"", "t":"1990-12-05T12:35", "c":"Message contents"},
+	{"id":"3",	"f":"lancelot@camelot.com",	"h":"Please spend some time with me.", "l":"meeting today", "a":"", "t":"1990-12-05T12:35", "c":"Message contents"},
 	{"id":"4",	"f":"lancelot@camelot.com",	"h":"I think I love you.", "l":"meeting today", "a":"", "t":"1990-12-05T12:36", "c":"Message contents"},
 	{"id":"5",	"f":"meliagaunt@camelot.com",	"h":"I know what you did last summer.", "l":"meeting today", "a":"", "t":"1990-12-05T12:37", "c":"Message contents"},
 	{"id":"6",	"f":"lancelot@camelot.com",	"h":"What did you tell Meliagaunt?", "l":"meeting today", "a":"", "t":"1990-12-05T12:38", "c":"Message contents"},
